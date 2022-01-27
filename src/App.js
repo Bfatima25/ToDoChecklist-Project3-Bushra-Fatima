88fefd000fa41b55e3e4091615315798ab923422
Bfatima25 <bushrafatima102@gmail.com>
2022-01-26 22:15:23 -0600

    edited few more things in App.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import FormToDo from "./Components/FormToDo";
 import ToDoList from "./Components/ToDoList";
 
 function App() {
+  //state
+  const [inputText, setInputText] = useState("");
+  const [doslist, setDolist] = useState([]);
   return (
     <div className="ToDolist">
       <header>
         <h1>What's The Plan Today?</h1>
       </header>
+      <FormToDo inputText={inputText}  doslist={doslist} setDolist={setDolist} setInputText={setInputText} setStatus={setStatus} />
+      <ToDoList setDolist={setDolist} doslist={doslist} filteredDoslist={filteredDoslist} />
     </div>
   );
 }
