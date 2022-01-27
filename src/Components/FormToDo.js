09afe22369c28ede8f220674bdfc4347eb811410
Bfatima25 <bushrafatima102@gmail.com>
2022-01-26 23:26:04 -0600

    added check and delete handler in FormToDo.js

--- src/Components/FormToDo.js
+++ src/Components/FormToDo.js
@@ -1,14 +1,27 @@
 import React from 'react';
 
-function FormToDo () {
+function FormToDo ({setInputText, doslist, setDolist, inputText, setStatus}) {
+    const inputDoHandler = (e) => {
+        setInputText(e.target.value);
+    };
+    const submitDoHandler  = (e) => {
+        e.preventDefault();
+        setDolist([
+            ...doslist, {text: inputText, completed: false, id: Math.random()*1000 },
+        ]);
+        setInputText("");
+    };
+    const statusHandler = (e) => {
+        setStatus(e.target.value);
+    };
     return(
         <form>
-            <input type="text" className="dolist-input" />
-            <button className="dolist-button" type="submit">
+            <input value={inputText} onChange={inputDoHandler} type="text" className="dolist-input" />
+            <button onClick={submitDoHandler} className="dolist-button" type="submit">
                 <i className="fas fa-plus-circle"></i>
             </button>
             <div className="select">
-                <select name="list" className="filter-dolist">
+                <select onChange={statusHandler} name="list" className="filter-dolist">
                     <option value="all">All</option>
                     <option value="completed">Completed</option>
                     <option value="incomplete">InComplete</option>
@@ -19,4 +32,3 @@ function FormToDo () {
 }
 
 export default FormToDo;
-}
